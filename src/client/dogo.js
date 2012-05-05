30a090e72cb28ba283e1b48648e8ac0611cf2295
Marcus Westin <marcus.westin@gmail.com>
2012-05-05 13:44:55 -0700

    Fix back button showing

--- src/client/dogo.js
+++ src/client/dogo.js
@@ -78,7 +78,7 @@ function renderApp() {
 	app=div('app', viewport.fit,
 
 		scroller.renderHead(45, function(view, viewBelow, fromView) {
-			var showBackButton = viewBelow && (scroller.stack.length > 2)
+			var showBackButton = viewBelow && (scroller.stack.length > (scroller.hasConnectView ? 2 : 1))
 			return div('head',
 				showBackButton && renderBackButton(viewBelow.title || 'Home'),
 				div('title', view.title || 'Dogo'),
