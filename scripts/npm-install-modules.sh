08d4a0c76a4578babbd19721e75faedd1988252b
Marcus Westin <marcus.westin@gmail.com>
2012-05-20 18:40:11 -0700

    better feedback

--- scripts/npm-install-modules.sh
+++ scripts/npm-install-modules.sh
@@ -1,8 +1,8 @@
 for npm_module in uuid mysql request optimist aws2js knox express color
 do
+	echo "check npm install $npm_module"
 	if [ ! -f node_modules/$npm_module/.__npm_installed__ ]
 	then
-		echo "check npm install $npm_module"
 		cd node_modules/$npm_module && npm install . --production && touch .__npm_installed__ && cd ../..
 	fi
 done
