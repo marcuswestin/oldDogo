35e057fea600491c6c64dfaecba12760718d1c68
Marcus Westin <marcus.westin@gmail.com>
2012-07-28 14:09:05 -0700

    script to try running the current nginx script locally. Doesn't actually work serving files, but checks syntax before deploy

---
+++ src/scripts/run-nginx-dev.sh
@@ -0,0 +1,6 @@
+killall nginx
+cat src/server/config/nginx.conf | sed "s/\/var/\/usr\/local\/var/" | sed "s/\/etc/\/usr\/local\/etc/" | sed 's/listen 80/listen 8080/' > /usr/local/etc/nginx/nginx.conf
+mkdir -p /usr/local/var/log/nginx
+touch /usr/local/var/log/nginx/access.log
+touch /usr/local/var/log/nginx/error.log
+/usr/local/sbin/nginx
