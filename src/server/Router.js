5aa6c93b0fc9d9651f6f3ea0d3bc8e42d204e8c6
Marcus Westin <marcus.westin@gmail.com>
2012-07-28 10:10:14 -0700

    will this fix dogo?

--- src/server/Router.js
+++ src/server/Router.js
@@ -18,10 +18,7 @@ module.exports = proto(null,
 		this.messageService = messageService
 		this.pictureService = pictureService
 		this._router = express()
-		this._server = http.createServer(function(req, res) {
-			console.log("HERE", req.url)
-			res.end('pong')
-		})
+		this._server = http.createServer(this._router)
 		this._configureRouter(_opts || {})
 		if (_opts.dev) {
 			this._setupDev(this._router, this._server)
