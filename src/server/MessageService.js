c06197f851162a6248980eda1601f62efd6d51d1
Marcus Westin <marcus.westin@gmail.com>
2012-07-30 09:04:37 -0700

    disable invites for now

--- src/server/MessageService.js
+++ src/server/MessageService.js
@@ -44,7 +44,7 @@ module.exports = proto(null,
 						this._createMessage(accountId, toAccountId, conversation.id, body, pictureId, bind(this, function(err, message) {
 							if (err) { return callback(err) }
 							this.pushService.sendMessagePush(message, accountId, toAccountId, prodPush)
-							callback(null, { message:message, toAccountId:toAccountId, toFacebookId:toFacebookAccountId, disableInvite:false })
+							callback(null, { message:message, toAccountId:toAccountId, toFacebookId:toFacebookAccountId, disableInvite:true })
 						}))
 					})
 					
