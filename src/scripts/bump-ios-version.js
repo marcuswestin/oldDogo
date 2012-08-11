248f58040385c6cb82b5e9a65a18f2a8ff9a1c0c
Marcus Westin <marcus.westin@gmail.com>
2012-08-10 20:40:17 -0700

    fix bump script to output the new version instead of the old one

--- src/scripts/bump-ios-version.js
+++ src/scripts/bump-ios-version.js
@@ -6,10 +6,11 @@ var filename = __dirname+'/../client/ios/dogo/dogo-Info.plist'
 var lines = fs.readFileSync(filename).toString().split('\n')
 for (var i=0; i<lines.length; i++) {
 	if (lines[i].match('CFBundleShortVersionString') || lines[i].match('CFBundleVersion')) {
-		var version = lines[i+1].match(/<string>(.*)<\/string>/)[1]
-		lines[i+1] = lines[i+1].replace(version, semver.inc(version, 'patch'))
+		var currentVersion = lines[i+1].match(/<string>(.*)<\/string>/)[1]
+		var newVersion = semver.inc(currentVersion, 'patch')
+		lines[i+1] = lines[i+1].replace(currentVersion, newVersion)
 	}
 }
 fs.writeFileSync(filename, lines.join('\n'))
 
-console.log(version)
+console.log(newVersion)
