a05f51ac234a41c357852a7ffd91365cfd20cada
Marcus Westin <marcus.westin@gmail.com>
2012-08-18 16:51:38 -0700

    make the session-creation script work again

--- src/scripts/create-session.js
+++ src/scripts/create-session.js
@@ -1,11 +1,11 @@
-require('./server/util/globals')
+require('../server/util/globals')
 
-var SessionService = require('./src/server/SessionService')
+var SessionService = require('../server/SessionService')
 
 var accountId = process.argv[2]
 
 new SessionService().createSessionForAccountId(accountId, function(err, authToken) {
 	if (err) { return console.error('error:', err) }
-	console.log("Created session with auth:", authToken)
+	console.log("Created session for account", accountId, "with authToken", authToken)
 	process.exit(0)
 })
