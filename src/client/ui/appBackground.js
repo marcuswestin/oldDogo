c64b3fbd090c46ac08e4c72886d249bbfd0f13c8
Marcus Westin <marcus.westin@gmail.com>
2012-11-19 06:47:50 -0800

    add a footer to the app background with links to TOS and PP

--- src/client/ui/appBackground.js
+++ src/client/ui/appBackground.js
@@ -3,6 +3,8 @@ module.exports = {
 	update:update
 }
 
+var mdot = function() { return $('<span> &middot; </span>') }
+
 var $bg
 function render() {
 	return $bg=$(div('appBackground', style({
@@ -13,6 +15,7 @@ function render() {
 
 function update(width) {
 	var top = 10
+	var linkStyle = style({ color:'#126EAF' })
 	$bg.empty().css({ width:width, marginTop:top, height:viewport.height()-top, background:'#222' }).append(
 		div('card',
 			div('face', style(face.backgroundStyle(gState.myAccount().facebookId, true))),
@@ -20,6 +23,13 @@ function update(width) {
 				div('name', gState.myAccount().name),
 				div('lastMessage', div(style({ fontStyle:'italic' }), 'This is you'))
 			)
+		),
+		div(
+			style({ width:'100%', position:'absolute', bottom:10, left:0, textAlign:'center', fontSize:12, color:'#ddd' }),
+			a(linkStyle, { href:'/terms', target:'_blank' }, 'Terms of Service'),
+			mdot().css({ fontSize:18, position:'relative', top:3 }),
+			a(linkStyle, { href:'/privacy', target:'_blank' }, 'Privacy Policy'),
+			br(), span(style({ textShadow:'0 1px 0 rgba(255, 255, 255, .3)' }), 'Made with love by Marcus Westin')
 		)
 	)
 }
