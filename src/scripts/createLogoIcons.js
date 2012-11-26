00ff6450c238599d84cfca7e7d0b8968a5c96b4b
Marcus Westin <marcus.westin@gmail.com>
2012-11-26 17:37:19 -0500

    Update icons to have a less green blue and more red orange, as well as simpify generation of smaller icon sizes by scripting image-magick generation of them

--- src/scripts/createLogoIcons.js
+++ src/scripts/createLogoIcons.js
@@ -1,15 +1,23 @@
 var fs = require('fs')
 var exec = require('child_process').exec
 
-exec('rm -f src/graphics/logoIcon/*@2x.png', function(err, stderr, stdout) {
+exec('rm -rf src/graphics/logoIcon && mkdir src/graphics/logoIcon', function(err, stderr, stdout) {
 	if (err || stderr) { throw (err || stderr) }
-	fs.readdirSync('src/graphics/logoIcon').forEach(function(file) {
-		var match = file.match(/logoIcon-(\d+x\d+)\.png/)
-		if (!match) { return }
-		var size = parseInt(match[1])
-		exec('cp src/graphics/logoIcon/'+file+' src/graphics/logoIcon/'+file.replace(match[1], (size/2)+'x'+(size/2)+'@2x'), function() {
-			console.log(size, 'done')
+	[8, 12, 16, 24, 32, 34, 48, 64, 68, 96, 128, 192, 256, 384, 512].forEach(function(size) {
+		var sizeName = size+'x'+size
+		var sizeNameRetina = (size/2)+'x'+(size/2)+'@2x'
+		var srcPath = 'src/graphics/logoIcon2.png'
+		var filePath = 'src/graphics/logoIcon/logoIcon-'+sizeName+'.png'
+		var resizeCommand = ['convert', srcPath, '-resize', sizeName, filePath].join(' ')
+		console.log('Command:', resizeCommand)
+		exec(resizeCommand, function(err, stderr, stdout) {
+			if (err || stderr) { throw err || stderr }
+			var copyCommand = ['cp', filePath, filePath.replace(sizeName, sizeNameRetina)].join(' ')
+			exec(copyCommand, function(err, stderr, stdout) {
+				if (err || stderr) { throw err || stderr }
+				console.log(sizeName, sizeNameRetina, 'done')
+			})
 		})
-	})	
+	})
 })
 
