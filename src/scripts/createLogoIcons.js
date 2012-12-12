4df3d632e16dd5848fd1b1468736a459c0e68a8a
Marcus Westin <marcus.westin@gmail.com>
2012-12-11 16:42:53 -0800

    should be using logoIcon.png, not logoIcon2

--- src/scripts/createLogoIcons.js
+++ src/scripts/createLogoIcons.js
@@ -6,7 +6,7 @@ exec('rm -rf src/graphics/logoIcon && mkdir src/graphics/logoIcon', function(err
 	[8, 12, 16, 24, 32, 34, 48, 64, 68, 96, 128, 192, 256, 384, 512].forEach(function(size) {
 		var sizeName = size+'x'+size
 		var sizeNameRetina = (size/2)+'x'+(size/2)+'@2x'
-		var srcPath = 'src/graphics/logoIcon2.png'
+		var srcPath = 'src/graphics/logoIcon.png'
 		var filePath = 'src/graphics/logoIcon/logoIcon-'+sizeName+'.png'
 		var resizeCommand = ['convert', srcPath, '-resize', sizeName, filePath].join(' ')
 		console.log('Command:', resizeCommand)
