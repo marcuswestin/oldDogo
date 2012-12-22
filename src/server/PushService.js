1c5dd5d5dc9a6c65060638d2bc76939917c57c3f
Marcus Westin <marcus.westin@gmail.com>
2012-12-22 08:21:48 -0800

    start including the client UID in the push notification payload

--- src/server/PushService.js
+++ src/server/PushService.js
@@ -46,7 +46,13 @@ module.exports = proto(null,
 				this.db.selectOne(this, this.sql.selectAccountFirstName+'WHERE id=?', [fromAccountId], function(err, fromAccountInfo) {
 					if (err) { return log.error("ERROR this.sql.selectAccountFirstName", fromAccountId) }
 					var notification = new apns.Notification()
-					notification.payload = { id:message.id, senderAccountId:message.senderAccountId, conversationId:message.conversationId, toDogoId:toAccountId }
+					notification.payload = {
+						id:message.id,
+						senderAccountId:message.senderAccountId,
+						conversationId:message.conversationId,
+						toDogoId:toAccountId,
+						clientUid:message.clientUid
+					}
 					notification.badge = 1
 					notification.sound = "vibrate.wav"
 					if (message.pictureId) {
