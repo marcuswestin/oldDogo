bd9c70912cc34ef7f1c49bab91c57ecab564de36
Marcus Westin <marcus.westin@gmail.com>
2013-01-16 09:54:10 -0800

    add missing scrollToTop file

---
+++ src/js/website/template/scrollToTop.js
@@ -0,0 +1,4 @@
+window.scrollTo(0, 1)
+setTimeout(function () { window.scrollTo(0, 1) }, 10)
+setTimeout(function () { window.scrollTo(0, 1) }, 100)
+setTimeout(function () { window.scrollTo(0, 1) }, 1000)
