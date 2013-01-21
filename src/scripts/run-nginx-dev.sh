a51b3122b4b53dd17e688b16be6d5e293e4b2b38
Marcus Westin <marcus.westin@gmail.com>
2013-01-21 10:41:14 -0800

    fix run-nginx-dev script

--- src/scripts/run-nginx-dev.sh
+++ src/scripts/run-nginx-dev.sh
@@ -1,5 +1,5 @@
 killall nginx
-cat src/server/config/nginx.conf | sed "s/\/var/\/usr\/local\/var/" | sed "s/\/etc/\/usr\/local\/etc/" | sed 's/listen 80/listen 8080/' > /usr/local/etc/nginx/nginx.conf
+cat src/js/server/config/nginx.conf | sed "s/\/var/\/usr\/local\/var/" | sed "s/\/etc/\/usr\/local\/etc/" | sed 's/listen 80/listen 8080/' > /usr/local/etc/nginx/nginx.conf
 mkdir -p /usr/local/var/log/nginx
 touch /usr/local/var/log/nginx/access.log
 touch /usr/local/var/log/nginx/error.log
