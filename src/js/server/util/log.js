d754dc873e068c7fc0fa0fa53d4ffdb289bbd390
Marcus Westin <marcus.westin@gmail.com>
2013-02-06 17:17:17 -0800

    make log() do info logging

--- src/js/server/util/log.js
+++ src/js/server/util/log.js
@@ -14,19 +14,20 @@ function enableDebugLoggin() { logDebug = true }
 function makeLog(name) {
 	var padLength = 18
 	var pad = new Array(padLength-name.length).join(' ')+'-'+' '
-	return _.extend(function logInfo() { logDebug && doLog(pad, name, 'info'.blue, getArgsString(arguments)) },
-		{
-			debug: function logDebug() { doLog(pad, name, 'debug'.cyan, getArgsString(arguments).cyan) },
-			info: function logInfo() { doLog(pad, name, 'info'.blue, getArgsString(arguments)) },
-			warn: function logWarn() { doLog(pad, name, 'WARN'.pink, getArgsString(arguments), true) },
-			error: function logError() { doLog(pad, name, 'ERRO'.red, getArgsString(arguments), true) },
-			alert: function alert() {
-				var message = getArgsString(arguments)
-				doLog(pad, name.red, 'ALRT'.red, message.red, true)
-				sendSms.alertAdmin(('ALERT! '+message).substr(0, 160))
-			}
+	
+	function logInfo() { doLog(pad, name, 'info'.blue, getArgsString(arguments)) }
+	
+	return _.extend(logInfo, {
+		info: logInfo,
+		debug: function logDebug() { logDebug && doLog(pad, name, 'debug'.cyan, getArgsString(arguments).cyan) },
+		warn: function logWarn() { doLog(pad, name, 'WARN'.pink, getArgsString(arguments), true) },
+		error: function logError() { doLog(pad, name, 'ERRO'.red, getArgsString(arguments), true) },
+		alert: function alert() {
+			var message = getArgsString(arguments)
+			doLog(pad, name.red, 'ALRT'.red, message.red, true)
+			sendSms.alertAdmin(('ALERT! '+message).substr(0, 160))
 		}
-	)
+	})
 }
 
 function n(number) { return number < 10 ? '0'+number : number }
