a6551f5e54fd72f24d834b8582be6aaaf6c49f5c
Marcus Westin <marcus.westin@gmail.com>
2013-02-06 17:03:29 -0800

    stop caching npm installation

--- src/scripts/npm-install-modules.sh
+++ src/scripts/npm-install-modules.sh
@@ -1,8 +1,8 @@
 for npm_module in uuid mysql request optimist aws2js express color semver toobusy bcrypt
 do
 	echo "check npm install $npm_module"
-	if [ ! -f node_modules/$npm_module/.__npm_installed__ ]
-	then
-		cd node_modules/$npm_module && npm install . --production && touch .__npm_installed__ && cd ../..
-	fi
+	# if [ ! -f node_modules/$npm_module/.__npm_installed__ ]
+	# then
+		cd node_modules/$npm_module && npm install . --production && cd ../../ # touch .__npm_installed__ && cd ../..
+	# fi
 done
