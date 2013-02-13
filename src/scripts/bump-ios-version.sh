a7cc1ecc5380643102f176dac8afdd90c1db6dd0
Marcus Westin <marcus.westin@gmail.com>
2013-02-12 20:54:35 -0800

    fix bump script

--- src/scripts/bump-ios-version.sh
+++ src/scripts/bump-ios-version.sh
@@ -2,5 +2,5 @@ set -e
 
 version=$(node src/scripts/bump-ios-version.js $1)
 echo $version
-git add src/client/ios/dogo/dogo-Info.plist
+git add src/ios/dogo/dogo-Info.plist
 git commit -m "ios-version $version"
